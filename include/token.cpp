f14a8f6e6a943513bfdfe74c138a1ceacee54390
zethcxx <207147323+zethcxx@users.noreply.github.com>
2025-04-12 15:39:03 -0500

    feat(token): add Token class structure and initial implementtion
    Version: 0.02
    
    - Created `Token` class in `token.hpp` and `token.cpp`
    - Added support for token type, value, line, and column

--- include/token.cpp
+++ include/token.cpp
@@ -1,1 +1,45 @@
 #include "token.hpp"
+
+std::string_view Token::get_typestr( std::optional<Type> token_type ) const {
+    using enum Type;
+
+    switch ( token_type.value_or( this->type )) {
+        case BEGIN_FILE     : return "BEGIN_FILE"     ;
+        case IDENTIFIER     : return "IDENTIFIER"     ;
+        case STRING         : return "STRING"         ;
+        case UNCLOSED_STRING: return "STRING_INVALD"  ;
+        case NUMERIC        : return "NUMERIC"        ;
+        case NEWLINE        : return "NEWLINE"        ;
+        case ASSIGN         : return "ASSIGN"         ;
+        case SYMBOL         : return "SYMBOL"         ;
+        case PATH_INDICATOR : return "PATH_INDICATOR" ;
+        case PATHS_BLOCK    : return "PATHS_BLOCK"    ;
+        case INDENT_TAB     : return "INDENT_TAB"     ;
+        case INDENT_SPACE   : return "INDENT_SPACE"   ;
+        case INDENT_MIXTED  : return "INDENT_MIXTED"  ;
+        case END_OF_FILE    : return "END_OF_FILE"    ;
+        default:
+            return "Unknow";
+    }
+}
+
+
+Token::Token (
+    const Token::Type &_type  ,
+    const std::size_t &_line  ,
+    const std::size_t &_column,
+    const std::string &_value
+)
+  : type   { _type   },
+    line   { _line   },
+    column { _column },
+    value  { _value  }
+{}
+
+
+Token::Token ()
+  : type   { Type::BEGIN_FILE },
+    line   { 1 },
+    column { 1 },
+    value  { 0 }
+{}
--- include/token.hpp
+++ include/token.hpp
@@ -1,1 +1,67 @@
 #pragma once
+
+#include <cstdint>
+#include <optional>
+#include <string>
+#include <string_view>
+
+class Token {
+    public:
+        enum class Type : std::uint8_t {
+            BEGIN_FILE     ,
+            IDENTIFIER     ,
+            STRING         ,
+            UNCLOSED_STRING,
+            NUMERIC        ,
+            NEWLINE        ,
+            ASSIGN         ,
+            SYMBOL         ,
+            PATH_INDICATOR ,
+            PATHS_BLOCK    ,
+            INDENT_TAB     ,
+            INDENT_SPACE   ,
+            INDENT_MIXTED  ,
+            END_OF_FILE
+        };
+
+
+        /* ------------ GET METHODS ------------ */
+        [[nodiscard]]
+        const std::string &get_value() const { return this->value; }
+
+
+        [[nodiscard]]
+        std::size_t get_line   () const { return this->line  ; }
+
+
+        [[nodiscard]]
+        std::size_t get_columm () const { return this->column; }
+
+
+        [[nodiscard]]
+        Token::Type get_type   () const { return this->type  ; }
+
+
+        [[nodiscard]]
+        std::string_view get_typestr (
+            std::optional<Type> token_type = std::nullopt
+        ) const;
+
+
+        /* ---------- CONSTRUCTORS ---------- */
+        Token (
+            const Token::Type &_type  ,
+            const std::size_t &_line  ,
+            const std::size_t &_column,
+            const std::string &_value
+        );
+
+        Token();
+
+    private:
+        /* ----- MEMBERS ----- */
+        const Token::Type type  ;
+        const std::size_t line  ;
+        const std::size_t column;
+        const std::string value ;
+};
