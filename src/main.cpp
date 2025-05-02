3807b095691664d13c62ef327b1b4a07e8a8295d
zethcxx <207147323+zethcxx@users.noreply.github.com>
2025-05-02 17:38:49 -0500

    feat: add initial test code for project stage 1 in src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
-#include <print>
+#include <iostream>
+#include "read_config/lexer.hpp"
+#include "read_config/parser.hpp"
 
 auto main() -> int
 {
+    /* ------------------------------------------------- */
+    /*                      Example                      */
+    /* ------------------------------------------------- */
+    Lexer  lexer  { "comprexxion.txt" };
+    Parser parser { lexer };
+
+    if (parser.has_errors())
+        return EXIT_FAILURE;
+
+    std::cout << "~~~~~~~~ CONFIG ~~~~~~~~~\n";
+    parser.print_config();
+
     return EXIT_SUCCESS;
 }
